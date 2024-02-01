c9789b9debc3d6f31bcd09af11ed2ec1646b8155
Sina Akhavi <SinaAkhaviSpecial1984@gmail.com>
2024-02-01 17:32:10 +0330

    completed methods and tested successfully

--- evolutionary_for_mahak.h
+++ evolutionary_for_mahak.h
@@ -18,6 +18,7 @@ SC_MODULE(mahak) {
     double population[6][9];
 
     double compute_price(int member_id) {
+        
         double weights_of_member[OBJECT_NUMS];
         memcpy(weights_of_member, population[member_id], OBJECT_NUMS * sizeof(double));
 
@@ -31,7 +32,6 @@ SC_MODULE(mahak) {
     }
 
     void initialize_population() {
-
         srand(time(NULL));
 
         int i, j;
@@ -43,7 +43,7 @@ SC_MODULE(mahak) {
             }
         }
     }
-    
+
     void print_population() {
         int i, j;
         
@@ -58,14 +58,6 @@ SC_MODULE(mahak) {
 
     }
 
-    void test() {
-        char str[50] = "";
-
-        sprintf(str, "Bye %d", 13);
-
-        cout << str << endl;
-    }
-
     void check_and_repair_max_weight(int member_id) {
         int i;
         for (i = 0; i < OBJECT_NUMS; i++) {
@@ -75,8 +67,119 @@ SC_MODULE(mahak) {
         }
     }
 
+    void pick_best_members_as_parents (int* best_members_ids) {
+        int parents_nums = 2;
+        // int* best_members_ids = (int*)malloc(parents_nums * sizeof(int)); 
+
+        int max1 = -1000000;
+        int max2 = -1000000;
+
+        int max1_id = -1;
+        int max2_id = -1;
+
+        int i;
+        for (i = 0; i < MEMBER_NUM; i++) {
+            double price_i = compute_price(i);
+            printf("member %d - price= %f\n", i, price_i);
+            if (price_i > max1) {
+                max2 = max1;
+                max1 = price_i;
+
+                max2_id = max1_id;
+                max1_id = i;
+            }
+            else if (price_i > max2) {
+                max2 = price_i;
+                max2_id = i;
+            }
+        }
+
+        best_members_ids[0] = max1_id;
+        best_members_ids[1] = max2_id;
+    }
+
+    void do_crossover(int m1_id, int m2_id) {
+        double* best_member1_weights = population[m1_id];
+        double* best_member2_weights = population[m2_id];
+
+        srand(time(NULL));
+        int crossover_point = rand() % (OBJECT_NUMS + 1);
+        printf("crossover point= %d\n", crossover_point);
+
+        int i;
+        for (i = crossover_point; i < OBJECT_NUMS; i++) {
+            double temp = best_member1_weights[i];
+            best_member1_weights[i] = best_member2_weights[i];
+            best_member2_weights[i] = temp;
+        }
+    }
+
+    void pick_best_u_individuals(int *best_u_member_ids, int u) {
+        int best_u_member_prices[u];
+        int i;
+
+        for (i = 0; i < u; i++) {
+            best_u_member_prices[i] = -100000;
+            best_u_member_ids[i] = -1;
+        }
+
+        for (i = 0; i < MEMBER_NUM; i++) {
+            double price_i = compute_price(i);
+
+            int j;
+            int flag = 1;
+            for (j = 0; j < u && flag == 1; j++) {
+                if  (price_i > best_u_member_prices[j]) {
+                    int q;
+                    for (q = u - 1; q >= j; q--) {
+                        if (q == j) {
+                            best_u_member_prices[q] = price_i;
+                            best_u_member_ids[q] = i;
+                            flag = -1;
+                            break;
+                        } else {
+                            best_u_member_prices[q] = best_u_member_prices[q - 1];
+                            best_u_member_ids[q] = best_u_member_ids[q - 1];
+                        }
+                    }
+                }
+            }
+        } 
+        
+        
+    }
+
+    void check_bag_limitation_and_repair_member(double* weights) {
+        
+        double sum = 0;
+        int i;
+        for (i = 0; i < OBJECT_NUMS; i++) {
+            sum += weights[i];
+        }
+
+        if (sum < BAG_LIMIT) 
+            return;
+        
+        for (i = 0; i < OBJECT_NUMS; i++) {
+            if (weights[i] > 0 && weights[i] < 0.5) 
+                weights[i] = 0;
+            else if (weights[i] > 0.5)
+                weights[i] -= 0.5;
+        }
+
+        return check_bag_limitation_and_repair_member(weights);
+    }
+
+    void check_bag_limitation_and_repair_population() {
+        int i;
+        for (i = 0; i < MEMBER_NUM; i++) {
+            check_bag_limitation_and_repair_member(population[i]);
+        }
+    }
+
     SC_CTOR(mahak) {
         // SC_METHOD(print_prices);
     }
-
+    
 };
+
--- test.cpp
+++ test.cpp
@@ -1,16 +1,118 @@
 #include <string.h>
-#include <evolutionary_for_mahak.h>
+#include "evolutionary_for_mahak.h"
 
 int sc_main(int argc, char* argv[]) {
+    // mahak evol1("evolutionary_strategy");
+    // evol1.initialize_population();
+    // evol1.print_population();
+    // ** test check_and_repair_max_weight **
 
-    // test check_and_repair_max_weight
-    mahak evol1("evolutionary_strategy");
-    evol1.initialize_population();
-    evol1.print_population();
+    // mahak evol1("evolutionary_strategy");
+    // evol1.initialize_population();
+    // evol1.print_population();
 
-    evol1.check_and_repair_max_weight(0);
-    printf("-----------------------------\n");
-    evol1.print_population();
+    // evol1.check_and_repair_max_weight(0);
+    // printf("-----------------------------\n");
+    // evol1.print_population();
+
+    // ** test pick_best_members_as_parents **
+
+    // int best_member_ids[] = evol1.pick_best_members_as_parents();
+    // int nums = 2;
+    // int best_member_ids[nums];
+
+    // evol1.pick_best_members_as_parents(best_member_ids);
+    // printf("best_member_ids: %d\n", best_member_ids[0]);
+    // printf("best_member_ids: %d\n", best_member_ids[1]);
+
+    // ** test do_crossover **
+    // int nums = 2;
+    // int best_member_ids[nums];
+
+    // evol1.pick_best_members_as_parents(best_member_ids);
+    // printf("best_member_ids: %d\n", best_member_ids[0]);
+    // printf("best_member_ids: %d\n", best_member_ids[1]);
+    
+    // evol1.do_crossover(best_member_ids[0], best_member_ids[1]);
+    // evol1.print_population();    
+
+    // ** test pick_best_u_individuals **
+    // int u = 4;
+    // double best_u_member_prices[u];
+    // evol1.pick_best_u_individuals(best_u_member_prices, u);
+    
+    // printf("--------------------------------------\n");
+
+    // int i;
+    // int num = 6;
+    // for (i = 0; i < num; i++) {
+    //     printf("member %d - price= %f\n", i, evol1.compute_price(i));
+    // }
+    
+
+    // // int i;
+    // printf("-----------------------------------\n");
+    // printf("best members: \n");
+    // for (i = 0; i < u; i++) {
+    //     printf("best member %d - price = %f\n", i, best_u_member_prices[i]);
+    // }
+    // printf("\n");
+
+    // ** test2 pick_best_u_individuals **
+    // int i;
+    // printf("prices of members: \n");
+    // for (i = 0; i < 6; i++) {
+    //     printf("member id = %d - price = %f\n", i, evol1.compute_price(i));
+    // }
+
+    // int u = 4;
+    // int best_u_member_ids[u];
+    // evol1.pick_best_u_individuals(best_u_member_ids, u);
+    
+    // printf("\n\best members of members: \n");
+    // for (i = 0; i < u; i++) {
+    //     printf("best member id = %d, price= %f\n", best_u_member_ids[i], 
+    //             evol1.compute_price(best_u_member_ids[i]));
+    // }
+
+    // printf("\n");
+
+    
+    // ** test check_bag_limitation_and_repair **
+    // int num = 9;
+    // double numbers[num] = {5, 2, 0.5, 1, 7, 4, 3, 0, 6};
+
+    // int i;
+    // for (i = 0; i < num; i++) {
+    //     printf("unmodified element %d = %f\n", i, numbers[i]);
+    // }
+    
+    // printf("-----------------------------------------\n");
+
+    // evol1.check_bag_limitation_and_repair_member(numbers);
+    // for (i = 0; i < num; i++) {
+    //     printf("modified element %d = %f\n", i, numbers[i]);
+    // }
+
+    // ** test2 check_bag_limitation_and_repair **
+    //     double (*popul)[9] = evol1.population;
+
+    //     int i;
+    //     int nums = 6;
+    //     for (i = 0; i < nums; i++) {
+    //         evol1.check_bag_limitation_and_repair_member(popul[i]);
+    //     }
+
+    // printf("------------------------------\n");
+
+    // evol1.print_population();
+
+    // ** test3 check_bag_limitation_and_repair **
+    // evol1.check_bag_limitation_and_repair_population();
+
+    // printf("\n-----------------------------------------\n");
+    // evol1.print_population();
+
 
     return 0;
 }
